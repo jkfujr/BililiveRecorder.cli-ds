66d115ffefac87417b4dabc6433e02042d6eac81
jkfujr <jkfujr@qq.com>
2023-04-15 18:41:35 +0800

    调整文件结构

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-# 引用api文件
+# 引用整理好的数据
 from config.data import get_data
 from config.data import get_all_data
 from config.data import format_data_single
@@ -16,7 +16,7 @@ from config.data import format_data_single
 app = FastAPI()
 
 # 静态文件
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+app.mount("/assets", StaticFiles(directory="./webui/assets"), name="assets")
 # html文件
 webui = Jinja2Templates(directory="webui")
 
@@ -55,7 +55,7 @@ async def get_bililiveRec_room(object_id: str):
 if __name__ == "__main__":
 
     uvicorn.run(
-        "run:app",
+        "main:app",
         host="0.0.0.0",
         port=9101,
         # ssl相关，不需要就注释掉
